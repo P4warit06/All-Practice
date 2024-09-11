6c08694fd96d373f6826dd4c7296034d0e2da0dd
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-11 18:11:14 +0700

    Create FizzBuzz.js
    FizzBuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,19 @@
+// Write The Function Name FizzBuzz to check the type of parameter and return Fizz if parameter mod 3 equal 0
+//  return Buzz if parameter mod 5 equal 0 
+// Otherwise  return FizzBuzz if parameter mod 15 equal 0  
+ 
+
+
+console.log(FizzBuzz(3))
+console.log(FizzBuzz(5))
+console.log(FizzBuzz(15))
+
+
+// Otherwise Create Another Function Name FizzBuzzArray 
+// to check from the previous  Function 
+// that recieve Array 
+
+
+
+console.log(FizzBuzz([2,3,4,5,6,7,8,9,10.15]))
+console.log(FizzBuzz([2,3,4,5,6,7,8,9,10.15]))
