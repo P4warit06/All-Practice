1f107a7d6130ce33770ec5d1b317aeafe6d12d6b
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-11 18:12:20 +0700

    Update FizzBuzz.js
    FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,7 +2,11 @@
 //  return Buzz if parameter mod 5 equal 0 
 // Otherwise  return FizzBuzz if parameter mod 15 equal 0  
  
+/*
+Write Code here 
 
+
+*/
 
 console.log(FizzBuzz(3))
 console.log(FizzBuzz(5))
@@ -12,6 +16,12 @@ console.log(FizzBuzz(15))
 // Otherwise Create Another Function Name FizzBuzzArray 
 // to check from the previous  Function 
 // that recieve Array 
+
+/*
+Write Code here 
+
+
+*/
 
 
 
