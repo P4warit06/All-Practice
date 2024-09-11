09f89b9903a93f9df51d7d46480eae174838d89a
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-11 18:18:01 +0700

    Create ReverseString.js
    ReverseString

---
+++ ReverseString.js
@@ -0,0 +1,16 @@
+// Write The Function Name reverseString that receive parameter And check it type is String Or Not  if is String  return a ReverseString 
+/*
+  Write Code Here 
+
+
+*/
+
+
+
+
+
+
+
+console.log(reverseString('hello')) //olleh
+console.log(reverseString('Javascript')) // tpircsavaj
+
